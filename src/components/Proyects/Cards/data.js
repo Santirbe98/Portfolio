fcc2a078c00e68faac96d4827982d7d43d072dc3
Santirbe98 <santirbe@gmail.com>
2023-10-02 12:20:32 -0300

    update(October 2/10/2023)

--- src/components/Proyects/Cards/data.js
+++ src/components/Proyects/Cards/data.js
@@ -1,17 +1,28 @@
 export const proyects = [
   {
     id: 1,
+    name: 'Entrepreneur and Co-Founder of HREF',
+    description:
+      'As a passionate entrepreneur and co-founder of HREF I am part of a dynamic team dedicated to creating cutting-edge digital solutions. "href" is a digital startup specializing in the design and development of high-impact websites, ranging from elegant landing pages to complete e-commerce systems.',
+    date: '2023...',
+    githubRepo: 'https://github.com/hrefsolutions',
+    state: 'in progress',
+
+    proyectImage: 'https://avatars.githubusercontent.com/u/145393564?v=4',
+  },
+  {
+    id: 2,
     name: 'Imagine Big - Full-Stack web developer',
     description:
       'Front-end development of web pages for specific clients. Design and development from scratch in back-end. Fulfillment of objectives through agile methodologies (SCRUM).',
     date: '2023...',
     githubRepo: 'https://github.com/imaginebigdev',
-    state: 'in progress',
+    state: 'finish',
     liveDemo: 'https://www.imaginebig.dev/',
     proyectImage: 'https://i.ibb.co/VC7VTvt/1653564769609.jpg',
   },
   {
-    id: 2,
+    id: 3,
     name: 'E-Comerce Exo-Otaku',
     description:
       'Participation in the development of an e-commerce project for a local store. In this project we used agile methodology (SCRUM). Once a week we shared the progress of the project with our Product Owner. In the FRONT we used React, Redux, MUI and other libraries; in the Back we used Node.js, Express, and for the DB we used PostgreSQL and Sequelize.',
@@ -22,7 +33,7 @@ export const proyects = [
     proyectImage: 'https://i.ibb.co/R6M346R/exootaku.png',
   },
   {
-    id: 3,
+    id: 4,
     name: 'Dogs app',
     description:
       'Development of a Web App using React for the Front End and Redux as state management. All components were developed with CSS without the use of external libraries. The SPA consumes data from an API ("the dog api" https://thedogapi.com/) through a Back End developed in Node.JS using Express, adding new functionalities to the original API.',
@@ -36,33 +47,44 @@ export const proyects = [
 export const proyects2 = [
   {
     id: 1,
-    name: 'Imagine Big - Full-Stack web developer',
+    name: 'Emprendedor y Cofundador de HREF',
     description:
-      'Desarrollo de front-end para paginas web para clientes específicos. Diseño y desarrollo desde cero en back-end. Cumplimiento de objetivos mediante metodologias agiles (SCRUM).',
+      'Como emprendedor apasionado y co-fundador de HREF soy parte de un equipo dinámico dedicado a la creación de soluciones digitales de vanguardia. "href" es un emprendimiento digital especializado en el diseño y desarrollo de sitios web de alto impacto, abarcando desde elegantes landing pages hasta completos sistemas de comercio electrónico.',
     date: '2023...',
-    githubRepo: 'https://github.com/imaginebigdev',
-    liveDemo: 'https://www.imaginebig.dev/',
+    githubRepo: 'https://github.com/hrefsolutions',
+
     state: 'in progress',
-    proyectImage: 'https://i.ibb.co/VC7VTvt/1653564769609.jpg',
+    proyectImage: 'https://avatars.githubusercontent.com/u/145393564?v=4',
   },
   {
     id: 2,
+    name: 'Imagine Big - Full-Stack web developer',
+    description:
+      'Desarrollo de full-stack para paginas web para clientes específicos. Diseño y desarrollo desde cero en back-end. Cumplimiento de objetivos mediante metodologias agiles (SCRUM).',
+    date: 'Marzo 2023 - Septiembre 2023',
+    githubRepo: 'https://github.com/imaginebigdev',
+    liveDemo: 'https://www.imaginebig.dev/',
+    state: 'finish',
+    proyectImage: 'https://i.ibb.co/VC7VTvt/1653564769609.jpg',
+  },
+  {
+    id: 3,
     name: 'E-Comerce Exo-Otaku',
     description:
       'Participación en el desarrollo de un proyecto de comercio electrónico para una tienda local. En este proyecto utilizamos metodología ágil (SCRUM).Una vez a la semana compartíamos los avances en el proyecto con nuestro Product Owner. En el FRONT utilizamos React, Redux, MUI y otras librerías; en el Back Node.js, Express, y para la BDPostgreSQL y Sequelize.',
-    date: 'January 2023',
+    date: 'Enero 2023',
     githubRepo: 'https://github.com/Santirbe98/PF-ExoOtaku',
 
     state: 'finish',
     proyectImage: 'https://i.ibb.co/R6M346R/exootaku.png',
   },
   {
-    id: 3,
+    id: 4,
     name: 'Dogs app',
     description:
       'Desarrollo de una App Web utilizando React para el Front End y Redux como state management. Todos los Componentes fueron desarrollados con CSS sin uso de librerías externas. La SPA consume datos de una API("the dog api" https://thedogapi.com/) a través de un Back End desarrollado en Node.JS utilizando Express, agregando nuevas funcionalidades a la API original.',
     githubRepo: 'https://github.com/Santirbe98/PI-Dogs',
-    date: 'November 2022',
+    date: 'Noviembre 2022',
     state: 'finish',
     proyectImage: 'https://i.ibb.co/BVH7yX1/Dise-o-pagina.png',
   },
